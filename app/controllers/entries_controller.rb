bb47ed6cd8dd06399a0c7e47ced494304222321c
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-24 14:09:53 -0600

    add delete functionality

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -32,6 +32,11 @@ class EntriesController < ApplicationController
   	end
   end
 
+  def destroy
+   	Entry.find(params[:id]).destroy
+   	redirect_to entries_path
+  end
+
   private
 
   def entry_params
